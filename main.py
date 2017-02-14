e5671a8fc7fea290c7cc21ff4c74464e2a656626
Mina Pêcheux <mina.pecheux@gmail.com>
2017-02-14 16:16:26 +0100

    Added the example of harmonic potential
    See the file's commentaries for details.

--- main.py
+++ main.py
@@ -14,49 +14,45 @@ from math import sin, pi
 
 
 # general variables
-x_min = 0
-x_max = 100
+x_min = -50
+x_max = 50
 n = 100       # number of subdivisions of the space
 delta_x = (x_max - x_min) / n
 
+# we make the space discrete
+x_values = np.arange(x_min, x_max, delta_x)
+
 # declaration of main matrices
 laplacian = 1/delta_x**2 * -2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1)
-V = np.zeros(n)     # null potential
-H = laplacian + V
+V0 = np.zeros(n)                                # null potential
+V1 = np.asarray([x ** 2 for x in x_values])     # harmonic potential
+# here you choose which potential you add:
+H = laplacian + V1
 # we get the eigen vectors of the H matrix
 # to get the eigen functions of the H operator
 eigen_values, eigen_vectors = np.linalg.eigh(H)
-
-# we make the space discrete
-x_values = np.arange(x_min, x_max, delta_x)
 
 # graphic representation of the first modes
 for i in range(0, 4):
     # we get the right psi values = the corresponding eigen vector
     # of the H matrix
 
-    # HOW IT WORKS:
-    # > the eigen vectors are COLUMN VECTORS!
-    # > and it gives you an oscillating curve, then you switch one point out of two to get back your sinus...
+    # THE PROBLEM WAS: the eigen vectors are COLUMN VECTORS!
+    y_values = eigen_vectors[:, i]           # using the H eigen vectors
+    # --------------------------------
+    # The following part is necessary for the null potential to get the sinus, because
+    # we have an oscillating curve and we need to switch one point out of two to get back the sinus...
     # (I don't know why?)
-    y_values = [sin((i+1)*pi*x/x_max) for x in x_values]  # analytic solution
-    y_values2 = eigen_vectors[:, i]                       # using the H eigen vectors
-    inverse_index = next((k for k, v in enumerate(y_values2) if v < 0), -1)
-    for j in range(inverse_index, len(y_values2), 2):
-        y_values2[j] = -y_values2[j]
-    # we plot the both results against the x values
+    # BUT it must removed for the harmonic potential
+    inverse_index = next((k for k, v in enumerate(y_values) if v < 0), -1)
+    for j in range(inverse_index, len(y_values), 2):
+        y_values[j] = -y_values[j]
+    # --------------------------------
+    # we plot the result against the x values
     plt.figure(1)
     plt.plot(x_values, y_values, label='mode %d' % (i+1))
-    plt.figure(2)
-    plt.plot(x_values, y_values2, label='mode %d' % (i+1))
 
 # we finish up the graphs and show them
-plt.figure(1)
-plt.legend()
-plt.xlabel('Horizontal position')
-plt.ylabel('Wave functions')
-
-plt.figure(2)
 plt.legend()
 plt.xlabel('Horizontal position')
 plt.ylabel('Wave functions')
