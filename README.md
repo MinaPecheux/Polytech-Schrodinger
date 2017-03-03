764183a85aafbe79b150183c6c5e804c1fecde9c
Mina Pêcheux <mina@MacMina.local>
2017-03-03 22:31:52 +0100

    Updated main and readme files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # [MAIN3] Projet Schrödinger
 This project was realized between January and June 2017 by Anida Khizar, Rahmatoulaye Ndiaye, Vincent Nicol and Mina Pêcheux.
+
+The study was divided in several parts:
+1) a 1-D box, with different potentials and a time-independent solution
+2) a 2-D box, with different potentials and a time-independent solution
+3) a 1-D box, with different potentials and a time-dependent solution
--- main.py
+++ main.py
@@ -18,59 +18,59 @@ x_min = -5.0
 x_max = 5.0
 n = 100       # number of subdivisions of the space
 delta_x = (x_max - x_min) / n
-h = 6.64E-34
+h = 6.64E-34 / (2*pi)       # Dirac's constant
+m = 1E-39
 
 # we make the space discrete
 x_values = np.arange(x_min, x_max, delta_x)
 
 # declaration of main matrices
-laplacian = -1 / (2 * delta_x**2) * (-2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1))
-V0 = np.zeros((n, n))                                                # null potential
-V1 = np.diag(np.asarray([x ** 2 for x in x_values]))                     # harmonic potential
-#V2 = np.asarray([abs(round(sin(3*x))) for x in x_values])       # rectangular potential
-V2 = np.diag(np.asarray([1 if abs(x) < x_max / 3 else 0 for x in x_values]))
+laplacian = - 1 / delta_x**2 * (-2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1))
+V0 = np.zeros((n, n))                                                   # null potential
+V1 = np.diag([x ** 2 for x in x_values])                                # harmonic potential
+V2 = np.diag([1.0 if abs(x) < 2.0 else 0 for x in x_values])    # rectangular potential
+V3 = np.diag([abs(x) for x in x_values])                                # absolute potential
 # here you choose which potential you add:
-H = laplacian + V0
+H = laplacian + V2
 # we get the eigen vectors of the H matrix
 # to get the eigen functions of the H operator
 eigen_values, eigen_vectors = np.linalg.eigh(H)
 
-# modes that we want to compute
-modes = range(0, 5)
+# energies that we want to compute
+energies = range(0, 5)
 
-# graphic representation of the first modes
+# graphic representation of the first energies
 # (not separated)
 plt.figure(1)       # we set the current figure
-lx = x_max-x_min
-for i in modes:
+for i in energies:
     # we get the right psi values = the corresponding eigen vector
     # of the H matrix (reminder: these are column vectors)
     y_values = eigen_vectors[:, i]
     # we plot the result against the x values
     plt.plot(x_values, y_values, label='energy level %d' % (i+1))
-plt.plot(x_values, [sqrt(2/lx)*sin(2*x*pi/lx)for x in x_values])
 
-# we finish up the graphs and show them
-plt.title('Wave functions depending on the mode')
+# we finish up the graph
+plt.title('Wave functions given the energy level')
 plt.legend()
 plt.xlabel('Horizontal position')
 plt.ylabel('Wave functions')
 
-# graphic representation of the first modes
+# graphic representation of the first energies
 # (separated by arbitrary offset)
 plt.figure(2)       # we set the current figure
 offset = 0.5
-for i in modes:
+for i in energies:
     # we get the right psi values = the corresponding eigen vector
     # of the H matrix (reminder: these are column vectors)
     y_values = [i*offset + v for v in eigen_vectors[:, i]]
     # we plot the result against the x values
     plt.plot(x_values, y_values, label='energy level %d' % (i+1))
 
-# we finish up the graphs and show them
-plt.title('Wave functions depending on the mode with an arbitrary offset = %.1f' % offset)
+# we finish up the graph
+plt.title('Wave functions given the energy level with an arbitrary offset = %.1f' % offset)
 plt.legend()
 plt.xlabel('Horizontal position')
 plt.ylabel('Wave functions')
 
+# we show the graphs
 plt.show()
