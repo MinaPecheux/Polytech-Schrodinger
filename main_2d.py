5b3360d5366271b135f5c10328a30a4342ff6aa5
Mina Pêcheux <mina@MacMina.local>
2017-03-09 15:35:17 +0100

    Modified script, but still NOT OK.

--- main_2d.py
+++ main_2d.py
@@ -1,10 +1,10 @@
 """
-File : main.py
+File : main_2d.py
 [MAIN3 - Schrodinger] Feb. - June 2017
 A. Khizar, R. Ndiaye, V. Nicol, M. Pecheux
 Referent: F. Aviat
 
-Main file that represents the wave function using the TISE.
+Main file that represents the wave functions in 2D using the TISE.
 """
 
 # useful imports
@@ -15,13 +15,13 @@ from math import sqrt
 
 
 # general variables
-x_min = -30
-x_max = 30
+x_min = -3
+x_max = 3
 lx = x_max - x_min
-y_min = -50
-y_max = 50
+y_min = -3
+y_max = 3
 ly = y_max - y_min
-n = 100     # number of subdivisions of the space
+n = 5     # number of subdivisions of the space
 delta_x = lx / n
 delta_y = ly / n
 h = 6.64E-34
@@ -32,18 +32,17 @@ y_values = np.arange(y_min, y_max, delta_y)
 X, Y = np.meshgrid(x_values, y_values)
 
 # declaration of main matrices
-laplacian_x = -h ** 2 / (2 * delta_x**2) * (-2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1))
-laplacian_y = -h ** 2 / (2 * delta_y**2) * (-2*np.eye(n) + np.diag(np.ones(n - 1), -1) + np.diag(np.ones(n - 1), 1))
-V0 = np.zeros(n)                                                # null potential
+laplacian_x = - 1 / delta_x**2 * (-2*np.eye(n*n) + np.diag(np.ones(n*n - 1), -1) + np.diag(np.ones(n*n - 1), 1))
+laplacian_y = - 1 / delta_y**2 * (-2*np.eye(n*n) + np.diag(np.ones(n*n - 3), 3) + np.diag(np.ones(n*n - 3), -3))
+laplacian = 1/2 * (laplacian_x + laplacian_y)
+V0 = np.zeros((n*n, n*n))                                                # null potential
 #V1 = np.asarray([x ** 2 for x in x_values])                     # harmonic potential
 #V2 = np.asarray([abs(round(sin(3*x))) for x in x_values])       # rectangular potential
 # here you choose which potential you add:
-H_x = laplacian_x + V0
-H_y = laplacian_y + V0
+H = laplacian + V0
 # we get the eigen vectors of the H matrix
 # to get the eigen functions of the H operator
-eigen_values_x, eigen_vectors_x = np.linalg.eigh(H_x)
-eigen_values_y, eigen_vectors_y = np.linalg.eigh(H_y)
+eigen_values, eigen_vectors = np.linalg.eigh(H)
 
 # we create the figures
 fig1, fig2 = plt.figure(1), plt.figure(2)
@@ -51,15 +50,21 @@ fig1, fig2 = plt.figure(1), plt.figure(2)
 ax1, ax2 = fig1.gca(projection='3d'), fig2.gca(projection='3d')
 
 # orbitals that we want to compute
+energy = 2
 nx, ny = 2, 2
 
 # analytic solution representation: psi^2 = presence probability
-Z = 2/sqrt(lx*ly) * np.multiply(np.sin(nx * np.pi * X/lx), np.sin(ny * np.pi * Y/ly))
+Z = (2/sqrt(lx*ly) * np.multiply(np.sin(nx * np.pi * X/lx), np.sin(ny * np.pi * Y/ly))) ** 2
+
 # we plot the result against the x values
 ax1.plot_surface(X, Y, Z, linewidth=0, antialiased=True)
 
 # experimental solution representation: psi^2 = presence probability
-Z = np.multiply(eigen_vectors_x[:, nx], eigen_vectors_y[:, ny])
+psi = list(eigen_vectors[:, energy - 1])
+Z = []
+for i in range(n):
+    Z.append([v ** 2 for v in psi[i*n:(i+1)*n]])
+
 ax2.plot_surface(X, Y, Z, linewidth=0, antialiased=True)
 
 plt.show()
